5cefdf589d942bd6f8e0faf2cc160883c3469c6e
vdsou <vdsouza@outlook.com>
2022-05-27 12:20:37 -0300

    :wrench: add data-source example

---
+++ src/config/data-source.example.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "test",
+  password: "test",
+  database: "testdb",
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+})
+;
