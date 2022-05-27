5e50fb9a0991997e1c793dc31e41cdc17603f73e
vdsou <vdsouza@outlook.com>
2022-05-27 13:17:02 -0300

    :recycle: format code

--- test/server.test.ts
+++ test/server.test.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-type Response = {
-  data: Object;
-};
+type Response = { data: Object };
 test("check if the server is up and running", async () => {
   const res = await axios.get<Response>("http://localhost:3000");
   expect(res.data).toBeTruthy();
