1a254d64ac287157553c80537af27d87578a0510
vdsou <vdsouza@outlook.com>
2022-06-20 16:49:56 -0300

    :sparkles: add user update validation

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,7 @@
 import express, { Router } from "express";
 import UserController from "../resourses/user/user.controller";
 import auth from "../middlewares/auth";
-import { validateSignIn, validateSignUp } from "../middlewares/validate";
+import { validateSignIn, validateSignUp, validateUserUpdate } from "../middlewares/validate";
 
 const userRouter: Router = express();
 const userController = new UserController();
@@ -10,6 +10,6 @@ userRouter.get("/me", auth, userController.me);
 userRouter.post("/signup", validateSignUp, userController.signUp);
 userRouter.post("/signin", validateSignIn, userController.signIn);
 userRouter.delete("/delete/:id", auth, userController.delete);
-userRouter.patch("/update", auth, userController.update);
+userRouter.patch("/update", auth, validateUserUpdate, userController.update);
 
 export default userRouter;
